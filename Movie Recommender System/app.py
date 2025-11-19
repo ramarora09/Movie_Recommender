1c7b5b6311d0c3b2a3a168b94546210e2d8889ab
Ram Prakash Arora <ramarora8707659@gmail.com>
2025-11-19 17:15:07 +0530

    Add files via upload
    Note: similarity.pkl (model similarity matrix) is not uploaded due to GitHub’s 25MB file size limit.

---
+++ Movie Recommender System/app.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import requests
+
+API_KEY = "5b1d0e0ec74e41a9b788c6a734f9da0f"
+
+def fetch_poster(movie_id):
+    poster_url = (
+        f"https://api.themoviedb.org/3/movie/{movie_id}"
+        f"?api_key={API_KEY}&language=en-US"
+    )
+
+    try:
+        response = requests.get(poster_url, timeout=3)
+        data = response.json()
+
+        if "poster_path" in data and data["poster_path"]:
+            return "https://image.tmdb.org/t/p/w500" + data["poster_path"]
+        else:
+            return "https://via.placeholder.com/500x750?text=No+Poster"
+
+    except:
+        return "https://via.placeholder.com/500x750?text=No+Poster"
+
+
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = similarity[index]
+    movie_list = sorted(list(enumerate(distances)),
+                        reverse=True, key=lambda x: x[1])[1:6]
+
+    recommended_movies = []
+    recommended_posters = []
+
+    for i in movie_list:
+        movie_id = movies.iloc[i[0]].id
+        recommended_movies.append(movies.iloc[i[0]].title)
+        recommended_posters.append(fetch_poster(movie_id))
+
+    return recommended_movies, recommended_posters
+
+
+movies = pickle.load(open("movies.pkl", "rb"))
+similarity = pickle.load(open("similarity.pkl", "rb"))
+
+st.title("Movie Recommender System")
+
+selected_movie = st.selectbox(
+    "Choose a movie:",
+    movies["title"].values
+)
+
+if st.button("Recommend"):
+    names, posters = recommend(selected_movie)
+
+    cols = st.columns(5)
+    for col, name, poster in zip(cols, names, posters):
+        col.text(name)
+        col.image(poster, width='stretch')
